1efbe0ee7d04f8ea869a22219ba5adfcf3d657c9
Chathurya Sudhakarreddy <chathurya901@gmail.com>
2025-06-28 02:22:00 -0500

    Submitting

--- AlphaBeta.py
+++ AlphaBeta.py
@@ -5,34 +5,29 @@ class Player(BasePlayer):
     def __init__(self, timeLimit):
         BasePlayer.__init__(self, timeLimit)
         self._startTime = 0
-        self._maxDepth = 3
+        self._maxDepth = 4  # Tune based on performance
 
     def findMove(self, state):
         self._startTime = time.time()
         legalMoves = state.actions()
-
         bestMove = None
         bestScore = float('-inf')
 
         for move in legalMoves:
             result = state.move(move)
-            if not self.timeRemaining():
-                break
             if result is None:
                 continue
-            score = self.minValue(result, self._maxDepth - 1)
+            score = self.minValue(result, self._maxDepth - 1, float('-inf'), float('inf'))
             if score is not None and score > bestScore:
                 bestScore = score
                 bestMove = move
 
-        # ✅ SAFE fallback in case bestMove is still None
         if bestMove is None and legalMoves:
-            print("[MyAgent] Fallback move:", legalMoves[0])
             bestMove = legalMoves[0]
 
         self.setMove(bestMove)
 
-    def maxValue(self, state, depth):
+    def maxValue(self, state, depth, alpha, beta):
         if state.gameOver() or depth == 0 or not self.timeRemaining():
             return state.getScore()
 
@@ -41,21 +36,27 @@ class Player(BasePlayer):
             result = state.move(move)
             if result is None:
                 continue
-            val = self.minValue(result, depth - 1)
+            val = self.minValue(result, depth - 1, alpha, beta)
             if val is None:
                 return None
             value = max(value, val)
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break  # Beta cut-off
         return value
 
-    def minValue(self, state, depth):
+    def minValue(self, state, depth, alpha, beta):
         if state.gameOver() or depth == 0 or not self.timeRemaining():
             return state.getScore()
 
         value = float('inf')
         for index, tile in state.possibleTiles():
             result = state.addTile(index, tile)
-            val = self.maxValue(result, depth - 1)
+            val = self.maxValue(result, depth - 1, alpha, beta)
             if val is None:
                 return None
             value = min(value, val)
+            beta = min(beta, value)
+            if beta <= alpha:
+                break  # Alpha cut-off
         return value
--- Game2048.py
+++ Game2048.py
@@ -3,180 +3,170 @@ import random
 import copy
 
 class Game2048:
-	def __init__(self, b=None, s=None):
-		if b:
-			self._board = b
-		else:
-			self._board = [0] * 16
-		
-		if s:
-			self._score = s
-		else:
-			self._score = 0
-		
-	def randomize(self):
-		self._board = []
-		for i in range(16):
-			self._board.append(random.choice([0]*16 + [1]*4 + [2]*2 + [3]))
+    def __init__(self, b=None, s=None):
+        if b:
+            self._board = b
+        else:
+            self._board = [0] * 16
 
-	def actions(self):
-		return ''.join([ a for a in 'UDLR' if self.move(a)._board != self._board ])
+        if s:
+            self._score = s
+        else:
+            self._score = 0
 
-	def result(self, a):
-		s = self._score
-		g = self.move(a)
-		zeros = [ i for i in range(16) if g._board[i] == 0 ]
-		i = random.choice(zeros)
-		if random.randint(0,3) == 3:
-			g._board[i] = 2
-		else:
-			g._board[i] = 1
-		return g, g._score - s
-		
-	def getScore(self):
-		return self._score
-		
-	def getTile(self, r, c):
-		return self._board[4*r+c]
+    def randomize(self):
+        self._board = []
+        for i in range(16):
+            self._board.append(random.choice([0]*16 + [1]*4 + [2]*2 + [3]))
 
-	def possibleResults(self, a):
-		s = self._score
-		possible = []
-		g = self.move(a)
-		zeros = [ i for i in range(16) if g._board[i] == 0 ]
-		for i in zeros:
-			g = self.move(a)
-			for t in [1,2]:
-				g._board[i] = t
-				if t == 1:
-					possible.append((g,.75/len(zeros)))
-				else:
-					possible.append((g,.25/len(zeros)))
-			
-		return possible
-		
-	def possibleTiles(self):
-		possible = []
-		zeros = [ i for i in range(16) if self._board[i] == 0 ]
-		for i in zeros:
-			for t in [1,2]:
-				possible.append((i,t))
-			
-		return possible
-		
-	def addTile(self, t, v):
-		g = copy.deepcopy(self)
-		g._board[t] = v
-		return g
+    def actions(self):
+        return ''.join([a for a in 'UDLR' if self.move(a)._board != self._board])
 
-	def move(self, action):
-		board = []
-		s = self._score
-		if action == 'R':
-			for i in range(0,16,4):
-				compressed = [t for t in self._board[i:i+4] if t != 0]
-				j = len(compressed) - 1
-				r = []
-				while j >= 0:
-					if j > 0 and compressed[j] == compressed[j-1]:
-						s += 2*(2**compressed[j])
-						r.insert(0,compressed[j]+1)
-						j -= 2
-					else:
-						r.insert(0,compressed[j])
-						j -= 1
-				r = [0] * (4-len(r)) + r					
-				board.extend(r)
-			return Game2048(board, s)
-		elif action == 'L':
-			for i in range(0,16,4):
-				compressed = [t for t in self._board[i:i+4] if t != 0]
-				j = 0
-				r = []
-				while j < len(compressed):
-					if j < len(compressed)-1 and compressed[j] == compressed[j+1]:
-						s += 2*(2**compressed[j])
-						r.append(compressed[j]+1)
-						j += 2
-					else:
-						r.append(compressed[j])
-						j += 1
-				r = r + [0] * (4-len(r))					
-				board.extend(r)
-			return Game2048(board, s)
-		elif action == 'D':
-			return self._flip().move('R')._flip()
-		elif action == 'U':
-			f = self._flip()
-			return self._flip().move('L')._flip()
-		else:
-			print('ERROR move =', action)
-				
-	def _flip(self):
-		r = []
-		for i in range(4):
-			r.extend( self._board[i:16:4] )
-		return Game2048(r, self._score)
-		
-	def rotate(self, numRotations):
-		numRotations = numRotations % 4
-		if numRotations == 0:
-			return Game2048(copy.copy(self._board), self._score)
-			
-		if numRotations == 1:
-			b = [0]*16
-			for r in range(4):
-				for c in range(4):
-					b[4*c + 3-r] = self._board[4*r+c]
-			return Game2048(b, self._score)
-			
-		if numRotations == 2:
-			b = [0]*16
-			for r in range(4):
-				for c in range(4):
-					b[4*(3-r) + 3-c] = self._board[4*r+c]
-			return Game2048(b, self._score)
-			
-		if numRotations == 3:
-			b = [0]*16
-			for r in range(4):
-				for c in range(4):
-					b[4*(3-c) + r] = self._board[4*r+c]
-			return Game2048(b, self._score)
-			
-	def gameOver(self):
-		return self.actions() == '' or 16 in self._board
+    def result(self, a):
+        s = self._score
+        g = self.move(a)
+        if g is None:
+            return self, 0
+        zeros = [i for i in range(16) if g._board[i] == 0]
+        if not zeros:
+            return g, g._score - s
+        i = random.choice(zeros)
+        g._board[i] = 2 if random.randint(0, 3) == 3 else 1
+        return g, g._score - s
 
-	def __str__(self):
-		s = ''
-		for r in range(0,16,4):
-			s += ' '.join(f'{2**x} '.rjust(5) for x in self._board[r:r+4]).replace(' 1 ','   ') + '\n'
-		s += f'Score = {self._score}'
-		return s
-		
+    def getScore(self):
+        return self._score
+
+    def getTile(self, r, c):
+        return self._board[4*r+c]
+
+    def possibleResults(self, a):
+        s = self._score
+        possible = []
+        g = self.move(a)
+        if g is None:
+            return []
+        zeros = [i for i in range(16) if g._board[i] == 0]
+        for i in zeros:
+            for t in [1, 2]:
+                new_g = copy.deepcopy(g)
+                new_g._board[i] = t
+                prob = 0.75/len(zeros) if t == 1 else 0.25/len(zeros)
+                possible.append((new_g, prob))
+        return possible
+
+    def possibleTiles(self):
+        possible = []
+        zeros = [i for i in range(16) if self._board[i] == 0]
+        for i in zeros:
+            for t in [1, 2]:
+                possible.append((i, t))
+        return possible
+
+    def addTile(self, t, v):
+        g = copy.deepcopy(self)
+        g._board[t] = v
+        return g
+
+    def move(self, action):
+        board = []
+        s = self._score
+        if action == 'R':
+            for i in range(0, 16, 4):
+                compressed = [t for t in self._board[i:i+4] if t != 0]
+                j = len(compressed) - 1
+                r = []
+                while j >= 0:
+                    if j > 0 and compressed[j] == compressed[j-1]:
+                        s += 2*(2**compressed[j])
+                        r.insert(0, compressed[j]+1)
+                        j -= 2
+                    else:
+                        r.insert(0, compressed[j])
+                        j -= 1
+                r = [0] * (4-len(r)) + r
+                board.extend(r)
+            return Game2048(board, s)
+
+        elif action == 'L':
+            for i in range(0, 16, 4):
+                compressed = [t for t in self._board[i:i+4] if t != 0]
+                j = 0
+                r = []
+                while j < len(compressed):
+                    if j < len(compressed)-1 and compressed[j] == compressed[j+1]:
+                        s += 2*(2**compressed[j])
+                        r.append(compressed[j]+1)
+                        j += 2
+                    else:
+                        r.append(compressed[j])
+                        j += 1
+                r = r + [0] * (4-len(r))
+                board.extend(r)
+            return Game2048(board, s)
+
+        elif action == 'D':
+            return self._flip().move('R')._flip()
+
+        elif action == 'U':
+            return self._flip().move('L')._flip()
+
+        else:
+            print('ERROR move =', action)
+            return None
+
+    def _flip(self):
+        r = []
+        for i in range(4):
+            r.extend(self._board[i:16:4])
+        return Game2048(r, self._score)
+
+    def rotate(self, numRotations):
+        numRotations = numRotations % 4
+        b = [0] * 16
+        for r in range(4):
+            for c in range(4):
+                if numRotations == 0:
+                    b[4*r + c] = self._board[4*r + c]
+                elif numRotations == 1:
+                    b[4*c + 3 - r] = self._board[4*r + c]
+                elif numRotations == 2:
+                    b[4*(3 - r) + (3 - c)] = self._board[4*r + c]
+                elif numRotations == 3:
+                    b[4*(3 - c) + r] = self._board[4*r + c]
+        return Game2048(b, self._score)
+
+    def gameOver(self):
+        return self.actions() == '' or 16 in self._board
+
+    def __str__(self):
+        s = ''
+        for r in range(0, 16, 4):
+            s += ' '.join(f'{2**x} '.rjust(5) for x in self._board[r:r+4]).replace(' 1 ', '   ') + '\n'
+        s += f'Score = {self._score}'
+        return s
+
 class BasePlayer:
-	def __init__(self, timeLimit):
-		self._timeLimit = timeLimit
-		self._startTime = 0
-		self._move = None
+    def __init__(self, timeLimit):
+        self._timeLimit = timeLimit
+        self._startTime = 0
+        self._move = None
 
-	def timeRemaining(self):
-		if time.time() < self._startTime + self._timeLimit:
-			return True
-		return False
+    def timeRemaining(self):
+        return time.time() < self._startTime + self._timeLimit
 
-	def setMove(self, move):
-		if self.timeRemaining():
-			self._move = move
+    def setMove(self, move):
+        if self.timeRemaining():
+            self._move = move
 
-	def getMove(self):
-		return self._move
+    def getMove(self):
+        return self._move
 
-	def stats(self):
-		pass
-		
-	def saveData(self, filename):
-		pass
-		
-	def loadData(self, filename):
-		pass
+    def stats(self):
+        pass
+
+    def saveData(self, filename):
+        pass
+
+    def loadData(self, filename):
+        pass
